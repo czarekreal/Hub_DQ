83019a8688501ea7eb5db6fa546bd1da74277d89
__czarekk__ <117777211+czarekkreal@users.noreply.github.com>
2024-01-28 00:52:52 +0100

    Update disable_core_gui.lua

--- LocalScripts/disable_core_gui.lua
+++ LocalScripts/disable_core_gui.lua
@@ -4,7 +4,7 @@ local plr = game.Players.LocalPlayer
 --disable core gui
 starter_gui:SetCoreGuiEnabled(Enum.CoreGuiType.All,false)
 
---loading gui faster than roblox do
+--loading gui faster than roblox do (need to bne inside of ReplicatedFirst to boost it's loading)
 for _,v in pairs(starter_gui:GetChildren()) do
 	v.Parent = plr
 end
